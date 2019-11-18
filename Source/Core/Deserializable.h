286bdd8071bf31900cf0be8c7a22a8c03ddcba48
Jack Bister <jack@jackbister.com>
2019-11-18 17:36:19 +0100

    clang-format Deserializable

--- Source/Core/Deserializable.h
+++ Source/Core/Deserializable.h
@@ -4,40 +4,41 @@
 #include "Core/Allocator.h"
 #include "Core/DllExport.h"
 #include "Core/HashedString.h"
-#include "Core/ResourceManager.h"
 
 class EAPI Deserializable
 {
 public:
-	/*
-		Serializes an object into a format suitable for storing in a scene file (currently JSON)
-	*/
-	virtual std::string Serialize() const = 0;
+    /*
+            Serializes an object into a format suitable for storing in a scene file (currently JSON)
+    */
+    virtual std::string Serialize() const = 0;
 
-	/*
-		Deserializes a string of unknown type.
-	*/
-	static Deserializable * DeserializeString(std::string const& str);
+    /*
+            Deserializes a string of unknown type.
+    */
+    static Deserializable * DeserializeString(std::string const & str);
 
-	static std::unordered_map <std::string, std::function<Deserializable *(std::string const&)>>& Map();
+    static std::unordered_map<std::string, std::function<Deserializable *(std::string const &)>> & Map();
 
-	std::string type;
+    std::string type;
 };
 
 #ifdef VK2D_DLL
 #define DESERIALIZABLE_IMPL(str, fn)
 #elif defined(VK2D_LIB)
-#define DESERIALIZABLE_IMPL(str, fn) static bool _##str##Instantiate() \
-							  { \
-									Deserializable::Map()[#str] = fn; \
-									return true; \
-							  } \
-							  extern "C" bool _##str##StaticInst = _##str##Instantiate();
+#define DESERIALIZABLE_IMPL(str, fn)                                                                                   \
+    static bool _##str##Instantiate()                                                                                  \
+    {                                                                                                                  \
+        Deserializable::Map()[#str] = fn;                                                                              \
+        return true;                                                                                                   \
+    }                                                                                                                  \
+    extern "C" bool _##str##StaticInst = _##str##Instantiate();
 #else
-#define DESERIALIZABLE_IMPL(str, fn) static bool _##str##Instantiate() \
-							  { \
-									Deserializable::Map()[#str] = fn; \
-									return true; \
-							  } \
-							  static bool _##str##StaticInst = _##str##Instantiate();
+#define DESERIALIZABLE_IMPL(str, fn)                                                                                   \
+    static bool _##str##Instantiate()                                                                                  \
+    {                                                                                                                  \
+        Deserializable::Map()[#str] = fn;                                                                              \
+        return true;                                                                                                   \
+    }                                                                                                                  \
+    static bool _##str##StaticInst = _##str##Instantiate();
 #endif
