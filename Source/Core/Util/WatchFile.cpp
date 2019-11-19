b2625ea1d8bf66db48e65980b838c21d43319200
Jack Bister <jack@jackbister.com>
2019-11-19 20:39:20 +0100

    Ensure file has changed in WatchFile

--- Source/Core/Util/WatchFile.cpp
+++ Source/Core/Util/WatchFile.cpp
@@ -11,14 +11,18 @@ static const auto logger = Logger::Create("WatchFile");
 #define WIN32_LEAN_AND_MEAN 1
 #include <Windows.h>
 #include <filesystem>
+#include <sys/stat.h>
 
 struct CallbackParam {
+    std::wstring filename;
+    time_t lastModified;
     HANDLE watchHandle;
     std::function<void()> callback;
     std::chrono::high_resolution_clock::time_point lastCall;
 
-    CallbackParam(HANDLE watchHandle, std::function<void()> callback)
-        : watchHandle(watchHandle), callback(callback), lastCall(std::chrono::high_resolution_clock::now())
+    CallbackParam(std::wstring filename, time_t lastModified, HANDLE watchHandle, std::function<void()> callback)
+        : filename(filename), lastModified(lastModified), watchHandle(watchHandle), callback(callback),
+          lastCall(std::chrono::high_resolution_clock::now())
     {
     }
 };
@@ -27,6 +31,14 @@ void WatchFileCallback(PVOID lpParameter, BOOLEAN TimerOrWaitFired)
 {
     auto arg = (CallbackParam *)lpParameter;
     auto now = std::chrono::high_resolution_clock::now();
+    struct _stat statResult;
+    _wstat(arg->filename.c_str(), &statResult);
+
+    if (statResult.st_mtime <= arg->lastModified) {
+        FindNextChangeNotification(arg->watchHandle);
+        return;
+    }
+    arg->lastModified = statResult.st_mtime;
     // Throttling, the callback gets called multiple times per change for some reason and the user should only get one
     // callback per change
     if (now - arg->lastCall < std::chrono::seconds(1)) {
@@ -45,12 +57,16 @@ void WatchFileCallback(PVOID lpParameter, BOOLEAN TimerOrWaitFired)
 void WatchFile_Windows(std::wstring filename, std::function<void()> onChange)
 {
     std::filesystem::path path(filename);
+	// You cannot use FindFirstChangeNotification on an individual file, so we look at the parent path and then stat the file in the callback.
     auto absolutePath = std::filesystem::absolute(path).parent_path();
 
     auto watchHandle = FindFirstChangeNotification(absolutePath.c_str(), false, FILE_NOTIFY_CHANGE_LAST_WRITE);
     HANDLE newWaitObject;
 
-    void * callbackArgs = new CallbackParam(watchHandle, onChange);
+    struct _stat statResult;
+    _wstat(filename.c_str(), &statResult);
+
+    void * callbackArgs = new CallbackParam(filename, statResult.st_mtime, watchHandle, onChange);
     auto registerResult = RegisterWaitForSingleObject(
         &newWaitObject, watchHandle, WatchFileCallback, callbackArgs, INFINITE, WT_EXECUTEDEFAULT);
 
